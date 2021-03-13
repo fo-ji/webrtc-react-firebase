ec2a61b43ff6f55693d308c5b18898fdff68d5db
fo-ji <57491651+fo-ji@users.noreply.github.com>
2021-03-13 12:11:09 +0900

    setup video component for remote

--- src/components/VideoArea.js
+++ src/components/VideoArea.js
@@ -25,9 +25,9 @@ const VideoArea = ({ localPeerName, remotePeerName }) => {
         <Grid item xs={12} sm={6}>
           <VideoLocal name={localPeerName} />
         </Grid>
-        {/* <Grid item xs={12} sm={6}>
-          <VideoRemote remotePeerName={remotePeerName} />
-        </Grid> */}
+        <Grid item xs={12} sm={6}>
+          <VideoRemote name={remotePeerName} />
+        </Grid>
       </Grid>
     </div>
   )
